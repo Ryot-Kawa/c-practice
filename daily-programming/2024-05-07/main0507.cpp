1d93d17d67b5f5f6edfcd8d8daf812c5d7d752f5
Ryot-Kawa <167525109+Ryot-Kawa@users.noreply.github.com>
2024-05-16 19:38:41 +0900

    Update daily-programming/2024-05-07/main0507.cpp
    Co-authored-by: ide-jun <83441165+ide-jun@users.noreply.github.com>

--- daily-programming/2024-05-07/main0507.cpp
+++ daily-programming/2024-05-07/main0507.cpp
@@ -6,9 +6,9 @@ int main(){
 if (0<n && n<=100 && 0<k && k<=100){
     int a[n];
     for(int i=0;i<n;i++){
-        cin >> a[n];
-        if((a[n]%k)==0){
-        cout << a[n]/k << " ";
+        cin >> a[i];
+        if((a[i]%k)==0){
+        cout << a[i]/k << " ";
         }
     }
     cout << endl;
