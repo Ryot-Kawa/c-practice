9a05219286d32748ac0aac8c464630d70f4c208a
Ryot-Kawa <ryk145599@gmail.com>
2024-05-16 12:44:19 +0900

    0516

---
+++ daily-programming/2024-05-16/main0516.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int a,b,c;
+  cin >> a >> b >> c;
+  if(1<=a,b,c && a,b,c<=13){
+    if(a+b+c>=22){
+      cout << "bust";
+    }
+    else{
+      cout << "win";
+    }
+  }
+}
