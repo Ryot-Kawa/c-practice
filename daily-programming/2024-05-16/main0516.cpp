e9abea092ab3631370d3a0b4239420609bdbb610
Ryot-Kawa <ryk145599@gmail.com>
2024-05-16 19:35:30 +0900

    0516

--- daily-programming/2024-05-16/main0516.cpp
+++ daily-programming/2024-05-16/main0516.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
   int a,b,c;
   cin >> a >> b >> c;
-  if (1<=a && a<=13 && 1<=b && b<=13 && 1<=c && c<=13)
+  if (1<=a && a<=13 && 1<=b && b<=13 && 1<=c && c<=13){
     if(a+b+c>=22){
       cout << "bust";
     }
